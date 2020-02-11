b504a3e8616d5346ce77678f257a7388b22621a6
aarazoo <54016292+aarazoo@users.noreply.github.com>
2020-02-11 11:10:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -37,7 +37,9 @@ class App extends React.Component {
   
   dataSubmit=(e)=>{
     e.preventDefault();
-    //console.log(this.state.name,this.state.age,this.state.croppedImageUrl,this.state.selectedOption)
+    if(this.state.name==="" || this.state.age==="" || this.state.croppedImageUrl==="" || this.state.selectedOption===""){
+      alert("please fill the feild.")
+    }else{
     let {imageArray} = this.state;
     let obj={
       id:this.state.imageArray.length + 1,
@@ -65,6 +67,7 @@ class App extends React.Component {
       selectedOption:"",
       imageArray
     })
+  }
   }
   
   addFile = (file, text) => {
